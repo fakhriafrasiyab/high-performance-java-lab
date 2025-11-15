bde286899126d75f3df53c11b838f14c8b213c5c
Faxri Afrasiyab <fakhriafrasiyab@gmail.com>
2025-11-15 08:26:37 +0100

    runner

--- jmh-benchmarks/src/main/java/com/fakhri/hpjava/BenchmarkRunner.java
+++ jmh-benchmarks/src/main/java/com/fakhri/hpjava/BenchmarkRunner.java
@@ -1,4 +1,16 @@
 package com.fakhri.hpjava;
 
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
 public class BenchmarkRunner {
+
+    public static void main(String[] args) throws Exception {
+        Options opt = new OptionsBuilder()
+                .include(StringConcatBenchmark.class.getSimpleName())
+                .build();
+        new Runner(opt).run();
+    }
+
 }

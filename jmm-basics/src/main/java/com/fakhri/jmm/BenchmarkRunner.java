4fc84797c10210d93da6d32dba727119e30aa88c
Faxri Afrasiyab <fakhriafrasiyab@gmail.com>
2025-11-15 12:04:59 +0100

    using 8 threads

--- jmm-basics/src/main/java/com/fakhri/jmm/BenchmarkRunner.java
+++ jmm-basics/src/main/java/com/fakhri/jmm/BenchmarkRunner.java
@@ -1,4 +1,21 @@
 package com.fakhri.jmm;
 
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
 public class BenchmarkRunner {
+
+    public static void main(String[] args) throws Exception {
+
+        Options opt = new OptionsBuilder()
+                .include(CounterBenchmark.class.getSimpleName())
+                .warmupIterations(2)
+                .measurementIterations(5)
+                .forks(2)
+                .build();
+
+        new Runner(opt).run();
+    }
+
 }
